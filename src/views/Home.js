00040ed1ede3c29673bd9f4c36f872ce3b0f7f73
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2021-04-27 19:49:29 -0500

    CRUD

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-function App() {
+export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Home</h1>
     </div>
   );
 }
-
-export default App;
---
+++ src/views/Students.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import firebase from 'firebase';
+import firebaseConfig from '../helpers/apiKeys';
+import '../App/App.scss';
+import StudentForm from '../StudentForm';
+import { getStudents } from '../helpers/data/studentData';
+import StudentCard from '../components/StudentCard';
+
+firebase.initializeApp(firebaseConfig);
+
+function App() {
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    getStudents().then((response) => setStudents(response));
+  }, []);
+
+  console.warn(students);
+
+  return (
+    <div className='App'>
+      <StudentForm
+      formTitle='Form Name'
+      setStudents={setStudents}
+      />
+        {students.map((studentInfo) => (
+          <StudentCard
+            key={studentInfo.firebaseKey}
+            firebaseKey={studentInfo.firebaseKey}
+            name={studentInfo.name}
+            teacher={studentInfo.teacher}
+            grade={Number(studentInfo.grade)}
+            setStudents={setStudents}
+          />
+        ))}
+    </div>
+  );
+}
+
+export default App;
